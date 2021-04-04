c0382112885082d9d22e48f1b73d56f03ec61576
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-04-04 20:30:58 +0200

    new build script

--- release.sh
+++ release.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-./gradlew clean
-
-./gradlew releaseMinor || exit 1
-
-git reset --hard HEAD~1
+./gradlew releaseMinor
 ./publish-npm.sh
-git pull
-
-./build.sh
-git commit -a --amend --no-edit
-git push -f
 
