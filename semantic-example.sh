dfc8438949f7afda1f67e82ac2661a9477fd37f1
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-05-25 16:11:10 +0200

    working on example

--- semantic-example.sh
+++ semantic-example.sh
@@ -2,8 +2,6 @@
 # cd html-validate
 
 highestTag=$(npx git-changelog-command-line \
- --major-version-pattern "^[Bb]reaking.*" \
- --minor-version-pattern "^[Ff]eat.*" \
  --print-highest-version-tag)
 
 echo Last release detected as $highestTag
@@ -15,45 +13,50 @@ nextVersion=$(npx git-changelog-command-line \
 
 echo Next release based on commits is: $nextVersion
 
-echo Changelog would be prepended with:
+echo Changelog:
 
 npx git-changelog-command-line \
- --from-ref $highestTag \
  --to-ref HEAD \
  --stdout \
  --template-content "
+# Changelog
 
-{{#hasType commits type='feat'}}
+{{#tags}}
+{{#isReleaseTag .}}
+## [{{name}}](https://gitlab.com/html-validate/html-validate/compare/{{name}}) ({{tagTime}})
+	{{#hasType commits type='feat'}}
 ### Features
-  {{#commits}}
-    {{#isType messageTitle type='feat'}}
-      {{printCommit .}}
-    {{/isType}}
-  {{/commits}}
+		{{#commits}}
+		{{#isType messageTitle type='feat'}}
+{{printCommit .}}
+		{{/isType}}
+		{{/commits}}
 
-{{/hasType}}
+	{{/hasType}}
 
-{{#hasType commits type='fix'}}
+	{{#hasType commits type='fix'}}
 ### Bug Fixes
-  {{#commits}}
-    {{#isType messageTitle type='fix'}}
+		{{#commits}}
+		{{#isType messageTitle type='fix'}}
 {{printCommit .}}
-    {{/isType}}
-  {{/commits}}
+		{{/isType}}
+		{{/commits}}
 
-{{/hasType}}
+	{{/hasType}}
 
 
-{{#hasType commits type='chore'}}
+	{{#hasType commits type='chore'}}
 ### Chores
-  {{#commits}}
-    {{#isType messageTitle type='chore'}}
+		{{#commits}}
+		{{#isType messageTitle type='chore'}}
 {{printCommit .}}
-    {{/isType}}
-  {{/commits}}
+		{{/isType}}
+		{{/commits}}
 
-{{/hasType}}
+	{{/hasType}}
 
+{{/isReleaseTag}}
+{{/tags}}
 " \
 --register-handlebars-helper "
 Handlebars.registerHelper('hasType', function(commits, options) {
@@ -67,16 +70,52 @@ Handlebars.registerHelper('hasType', function(commits, options) {
 });
 
 Handlebars.registerHelper('isType', function(messageTitle, options) {
-  const commitType = options.hash['type']
-  if (new RegExp('^' + commitType + '.*').test(messageTitle)) {
-    return options.fn(this);
-  } else {
-      return options.inverse(this);
-  }
+	const commitType = options.hash['type']
+	if (new RegExp('^' + commitType + '.*').test(messageTitle)) {
+		return options.fn(this);
+	} else {
+		return options.inverse(this);
+	}
+});
+
+Handlebars.registerHelper('isReleaseTag', function(tag, options) {
+	const name = tag.name
+	if (new RegExp('^v[0-9]+\\\\.[0-9]+\\\\.[0-9]+$').test(name)) {
+		return options.fn(this);
+	} else {
+		return options.inverse(this);
+	}
 });
 
 Handlebars.registerHelper('printCommit', function(commit, options) {
-  const message = commit.messageTitle.split(':')[1].trim()
-  return ' - ' + message + ' ([' + commit.hash + '](https://gitlab.com/html-validate/html-validate/commit/' + commit.hashFull + '))';
+	let type = new RegExp('^([^:\(]+)').exec(commit.messageTitle)
+	if (!type) {
+		return commit.messageTitle;
+	}
+	type = type[1];
+
+	let scope = '';
+	let scopes = new RegExp('^[a-z]+\\\\\\(([^\\\\\\)]+)').exec(commit.messageTitle)
+	if (scopes) {
+		scopes = scopes[1]
+		if (scopes.indexOf(':') == -1) {
+			scope = '**' + scopes + '**';
+		} else {
+			const parts = scopes.split(':');
+			for (let i = 0; i < parts.length; i++) {
+				scope += ' **' + parts[i] + '**'
+			}
+		}
+	}
+
+	let subject = commit.messageTitle
+	if (subject.indexOf('(refs ') != -1) {
+		subject = subject.substring(0,subject.indexOf('(refs '))
+	}
+	if (subject.indexOf('(fixes ') != -1) {
+		subject = subject.substring(0,subject.indexOf('(fixes '))
+	}
+
+	return ' - ' + scope + ' ' + subject + ' ([' + commit.hash + '](https://gitlab.com/html-validate/html-validate/commit/' + commit.hashFull + '))';
 });
 "
