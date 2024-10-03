cc8477d29799aa4e26ff5ce6820e251a8748bf76
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-10-03 21:45:19 +0200

    chore: building docker from local jars

--- build-docker.sh
+++ build-docker.sh
@@ -8,5 +8,5 @@ npx runnable-jar-to-docker@latest \
  --docker-password $dockerhub_token \
  --maven-group $group \
  --maven-artifact ${PWD##*/}  \
- --maven-version $(npx git-changelog-command-line --print-highest-version) \
+ --maven-version $(npx git-changelog-command-line@latest --print-highest-version) \
  --repository-url "file://$HOME/.m2/repository"
